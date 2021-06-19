ee2dc9ab71b073641b8005fe0b2a3bd6a1ad3f6e
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-19 04:57:30 -0700

    Two combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,11 +15,11 @@ for (i = 0; i < 10; i++)
 	for (j = i+1; j < 10; j++)
 	{
 		putchar(48 + j);
-		if (i != 8 && j != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	}
+	if (i != 8 && j != 9)
+	{
+		putchar(',');
+		putchar(' ');
 	}
 }
 putchar('\n');

a634d18aad2b29a0413004407a749d4abce1754a
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-19 04:52:30 -0700

    Two combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main block
+ *Description: two digit combo
+ *Return: 0
+ */
+
+int main(void)
+{
+int i,j;
+for (i = 0; i < 10; i++)
+{
+	putchar(48 + i);
+	for (j = i+1; j < 10; j++)
+	{
+		putchar(48 + j);
+		if (i != 8 && j != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}

b009823b94faece1a7115ebe7b144c0c7dd78391
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-19 03:38:36 -0700

    exclude e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,9 +12,11 @@ int i;
 char list[27] = "abcdefghijklmnopqrstuvwxyz";
 
 for (i = 0; i < 26; i++)
+{  
 	putchar(list[i]);
 	if (list[i] == 'q' || list[i] == 'e')
 		continue;
+}
 putchar('\n');
 return (0);
 }

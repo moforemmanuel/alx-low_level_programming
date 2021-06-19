74e8e3cb53a89c62f4ee822065c28e6d77a7255d
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-19 04:08:24 -0700

    rev alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *Description: return reverse string
+ *Return: 0
+ */
+
+int main(void)
+{
+a = 'z';
+while (a > 'a')
+{
+	putchar(a);
+	a--;
+}
+putchar('\n');
+return (0);
+}

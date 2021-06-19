e90a04b36dba49b4f0aa2a35e49959a0783587b4
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-19 02:55:45 -0700

    letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main block
+*return: 0
+*/
+
+int main(void)
+{
+  int i;
+  char list[26] = "abcdefghijklmnopqrstuvwxyx";
+  for (i=0; i<26; i++)
+    putchar(list[i])
+
+  return 0;
+}

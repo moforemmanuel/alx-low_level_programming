6648a6ef76ce23f73c83a3907dd456230fca391a
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-19 05:38:06 -0700

    Two combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,16 @@
 *Return: 0
 */
 
-int main(void){
-int i,j;
+int main(void)
+{
+int i, j;
 
 for (i = 0; i < 10; i++)
 {
-	for (j = i+1; j < 10;j++)
+	for (j = i + 1; j < 10; j++)
 	{
-		putchar(48+i);
-		putchar(48+j);
+		putchar(48 + i);
+		putchar(48 + j);
 		if (i == 8 && j == 9)
 		{
 			break;
@@ -27,5 +28,5 @@ for (i = 0; i < 10; i++)
 	}
 }
 putchar('\n');
-return 0;
+return (0);
 }

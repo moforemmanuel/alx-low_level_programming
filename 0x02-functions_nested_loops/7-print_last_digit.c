d3779a7db4260185ab4a5b0377605183f69c38bd
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-22 08:16:00 -0700

    last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,26 @@
 #include "holberton.h"
 /**
- * _abs - returns absolute value of n
+ * _abs- returns 1 if n is lowercase, 0 otherwise
  * @n: integer to be tested
- * Return: last digit
+ * Return: abs(n)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
+/**
+ * print_last_digit - pritns the last digit of a number
+ * @n: integer whose last digit is to be printed
+ * Return: lastdigit of n
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int lastDigit;
+
+	lastDigit = _abs((n % 10));
+	_putchar(lastDigit + 48);
+	return (lastDigit);
 }

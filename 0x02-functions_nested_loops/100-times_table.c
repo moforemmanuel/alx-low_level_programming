cbe1a1382b849db86aa991eba1972fb34e1d48be
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-22 09:22:24 -0700

    n time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * times_table - prints times table
+ */
+void print_times_table(int n)
+{
+	int i, j, prod;
+
+	if(!(n > 15 || n < 0))
+	{
+		for (i = 0; i < 10; i++)
+		{
+			for (j = 0; j < 10; j++)
+			{
+				if (j == 0)
+				{
+					_putchar(48);
+					continue;
+				}
+				prod = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (prod >= 10)
+				{
+					_putchar(prod / 10 + 48);
+					_putchar(prod % 10 + 48);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(prod + 48);
+				}
+			}
+		_putchar('\n');
+		}
+	}
+}

59d6296e247f80c88c5bd55705de03b84ccdaaa8
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-22 07:31:45 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _islower - returns 1 if c is lowercase, 0 otherwise
+ * @c: integer to be tested
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 122);
+}

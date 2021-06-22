4bf81ab6545159b2d1ca15a959a32d8a427d390c
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-22 07:54:36 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _abs - computes absolute value of an integer
+ * int: integer to be tested
+ * Return: abs
+ */
+int _abs(int)
+{
+int abs;
+if (int > 0)
+{
+	abs = int;
+}
+else if (int == 0)
+{
+	abs = 0;
+}
+else
+{
+	abs = int*-1;
+}
+return (abs);
+}

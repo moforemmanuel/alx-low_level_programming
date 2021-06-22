f07a697237853d9fd30c4d0490ce9913655346d4
moforemmanuel <moforemmanuel1000@gmail.com>
2021-06-22 06:51:01 -0700

    Print x10 alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10: print 10xthe alphabets
+ *Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+  int i = 0;
+  char c = 'a';
+  while (i < 10)
+    {
+      while (c <= 'z')
+	{
+	  _putchar(c);
+	  c++
+	    }
+      _putchar('\n');
+      i++;
+    }
+}

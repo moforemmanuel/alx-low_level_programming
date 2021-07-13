a4cb39a38f802675994a541aa599cccee1821937
moforemmanuel <moforemmanuel1000@gmail.com>
2021-07-12 22:37:25 -0700

    tasl 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints program name followed by new line
- * @argc : number of program parameters
- * @argv : array of pointers to string pointers of size argc
- * Return : 0
+ * main - prints program name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
 
-int main(int argc,  char const *argv[])
+int main(int argc, char const *argv[])
 {
 (void)argc;
 printf("%s\n", argv[0]);

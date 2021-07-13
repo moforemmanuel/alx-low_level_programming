b18d3c41c9daeb47b60a3a3129f6cdbe21dcc3b0
moforemmanuel <moforemmanuel1000@gmail.com>
2021-07-12 22:31:06 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatismyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name followed by new line
+ * @argc : number of program parameters
+ * @argv : array of pointers to string pointers of size argc
+ * Return : 0
+ */
+
+int main(int argc,  char cont *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}

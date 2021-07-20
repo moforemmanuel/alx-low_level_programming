c1909421fb0ea8374a7126d726bb3f349414a8fc
moforemmanuel <moforemmanuel1000@gmail.com>
2021-07-19 21:08:23 -0700

    dog ds

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,24 @@
-#idndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
- * struct dog - structure for dog
- * @name: name of dog
- * @age: age of dog
- * @owner: dog owner
- *
- * Description: This is a data structure that stores information about a dog
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
 
-struct dog
+typedef struct dog
 {
-  char *name;
-  float age;
-  char *owner;
-}
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
-#endif /*end DOG_H*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+#endif

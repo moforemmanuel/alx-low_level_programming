f38d0b9ac30aee013494e4757e664d0ea60bdfe2
moforemmanuel <moforemmanuel1000@gmail.com>
2021-07-19 20:55:59 -0700

    dog ds

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ *
+ * Description: This is a data structure that stores information about a dog
+ */
+
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+}

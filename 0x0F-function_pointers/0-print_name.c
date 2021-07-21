ec00cf54fd376443830c57468aa3653c425f891e
moforemmanuel <moforemmanuel1000@gmail.com>
2021-07-21 13:25:29 -0700

    0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,13 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: Nothing.
+ * print_name - prints a name
+ * @name: string
+ * @f: function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-int i = 0;
-
-while(name[i])
-{
-  _putchar(name[i]);
+if (f != NULL)
+	f(name);
 }
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,1 @@
-0x0F task
+# Function pointers

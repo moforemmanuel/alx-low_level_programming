c1978fc51c9618f3d67e90a175d609c4fa41b6b3
moforemmanuel <moforemmanuel1000@gmail.com>
2021-07-29 19:29:03 -0700

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    msg DB "Hello, Holberton",10      ; 10 is the ASCII code for a new line (LF)
-    msgSize EQU $ - msg
-
-global main
-
-section .text
-
+	global main
+	extern printf
 main:
-    mov rax, 4          ; function 4
-    mov rbx, 1          ; stdout
-    mov rcx, msg        ; msg
-    mov rdx, msgSize    ; size 
-    int 0x80
-    mov rax, 1          ; function 1
-    mov rbx, 0          ; code
-    int 0x80
-    ret
+	mov edi, format
+	xor eax, eax
+	call printf
+	mov eax, 0
+	ret
+format:	db `Hello, Holberton\n`,0

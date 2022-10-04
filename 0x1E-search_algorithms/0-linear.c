039a54e0c24933926df377a4526e2a18d4c6f36d
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-04 16:09:17 +0100

    search algos and 0-linear_search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,14 +10,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-  if (array == NULL)
-    return (-1);
+	if (array == NULL)
+		return (-1);
 
-  for (size_t i = 0; i < size; i++)
-  {
-    printf("Value checked array[%d] = [%d]\n", i, array[i]);
-    if (array[i] == value)
-      return (i);
-  }
-  return (-1);
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }

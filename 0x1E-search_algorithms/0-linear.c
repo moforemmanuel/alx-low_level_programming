ff15ab5350fffb7393c517490f08156c783fad89
Mofor Emmanuel <moforemmanuel1000@gmail.com>
2022-10-05 03:13:43 +0100

    search algos and 0-linear_search refact

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,23 +1,25 @@
 #include "search_algos.h"
 
 /**
- * linear_search - linear search algo
+ * linear_search - linear search implementation
  * @array: string
  * @size: size_t
  * @value: value
- * Return: int, index of value
+ * Return: int, index of the value
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
-		return (-1);
+size_t i;
 
-	for (size_t i = 0; i < size; i++)
-	{
-		printf("Value checked array[%li] = [%i]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
+if (array == NULL)
 	return (-1);
+
+for (i = 0; i < size; i++)
+{
+	printf("Value checked array[%li] = [%i]\n", i, array[i]);
+	if (array[i] == value)
+		return (i);
+}
+return (-1);
 }
--- 0x1E-search_algorithms/old/search_algos.h
+++ 0x1E-search_algorithms/old/search_algos.h
@@ -3,9 +3,8 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-void print_array(int *a, int i, int j);
 
 #endif
